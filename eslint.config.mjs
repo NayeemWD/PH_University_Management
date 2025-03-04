41c15ac571c49f57dbb56d2401c7a67e70b68eae
NayeemWD <msnayeem333@gmail.com>
2025-03-04 16:50:05 +0600

    fixt Eslint problem

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,32 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
 
-  {
-    ignores: ['node_modules', 'dist'],
-    rules: {
-      'no-unused-vars': 'error',
-      'no-unused-expressions': 'error',
-      'prefer-const': 'error',
-      'no-console': 'warn',
-      'no-undef': 'error',
+    {
+        ignores: ['node_modules', 'dist'],
+        plugins: {
+            prettier: eslintPluginPrettier,
+        },
+        rules: {
+            // 'no-unused-vars': 'error',
+            'no-unused-vars': 'off', // ❌ Disable built-in ESLint rule
+            '@typescript-eslint/no-unused-vars': 'error', // ✅ Use TypeScript's version
+            'no-unused-expressions': 'error',
+            'prefer-const': 'error',
+            'no-console': 'warn',
+            'no-undef': 'error',
+            'prettier/prettier': 'error',
+        },
     },
-  },
-  {},
-  eslintPluginPrettierRecommended,
-];
+
+    eslintConfigPrettier, // Ensures ESLint doesn't conflict with Prettier formatting
+]
